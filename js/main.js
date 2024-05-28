a9888a113ff7d0c56b4719d58646b5d8437ae5a1
Tendi Sambaza <baz@tendis-mbp.home>
2024-05-28 03:10:45 -0400

    Preloader and cleanup

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 
 //Delayed Foreword
 window.onload = function () {
-    var theDelay = 5;
-    setTimeout(showText, theDelay * 1000);
+    var loaderDelay = 3;
+    setTimeout(loaderPhaseOut, loaderDelay * 1000);
+    var preloaderDelay = 3.4;
+    setTimeout(preloaderPhaseOut, preloaderDelay * 1000);
+
+
 };
 
-function showText() {
-    document.getElementById("delayedForeword").style.visibility = "visible";
-}
+
+// function showText() {
+//     document.getElementById("delayedForeword").style.visibility = "visible";
+// }
 
 //Infinite Scrolling Tech Stack
 const scrollers = document.querySelectorAll(".scroller");
-
 // If a user hasn't opted in for recuded motion, then we add the animation
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
-
 function addAnimation() {
   scrollers.forEach((scroller) => {
     // add data-animated="true" to every `.scroller` on the page
@@ -36,3 +39,28 @@ function addAnimation() {
     });
   });
 }
+
+
+
+
+function loaderPhaseOut(){
+    document.getElementById("loader").style.visibility = "collapse";
+}
+function preloaderPhaseOut(){
+  document.getElementById("preloader").style.visibility = "collapse";
+}
+const radials = [...document.querySelectorAll('.radial')];
+let degrees = 0;
+for(i=0; i < radials.length; i++) {
+	degrees += 0;
+	radials[i].style.transform = `rotate(${degrees}deg)`;
+	degrees += 45;
+}
+radials.forEach((radial, index) => {
+	setTimeout(function() {
+		radial.classList.add('glow');
+	},index * 350);
+})
+
+;
+
